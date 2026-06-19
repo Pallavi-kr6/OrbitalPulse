610b1fd7f34ff3ee0ff87968095d68bb15a67868
pallavi <pallavi6315@gmail.com>
2026-06-19 20:35:42 +0530

    fix: add missing open-notify service

--- services/open-notify.ts
+++ services/open-notify.ts
@@ -1,9 +1,13 @@
+// services/open-notify.ts
 export async function getISSPosition() {
   const res = await fetch("http://api.open-notify.org/iss-now.json");
   const data = await res.json();
 
   return {
-    latitude: data?.iss_position?.latitude,
-    longitude: data?.iss_position?.longitude,
+    timestamp: data.timestamp,
+    iss_position: {
+      latitude: data.iss_position.latitude,
+      longitude: data.iss_position.longitude,
+    },
   };
 }
